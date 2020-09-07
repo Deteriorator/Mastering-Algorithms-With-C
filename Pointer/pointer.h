3768db5175395dd6f2f1060cb889fe0d23cb59a6
Leon <Liangz.org@gmail.com>
2020-09-07 23:33:05 +0800

    feat: use of pointer

---
+++ Pointer/pointer.h
@@ -0,0 +1,22 @@
+/*
+ *"""
+ *-----------------------------------------------------------------------------
+ * @Name：    pointer.h
+ * @Desc:     
+ * @Author:   liangz.org@gmail.com
+ * @Create:   2020.09.07   23:22
+ *-----------------------------------------------------------------------------
+ * @Change:   2020.09.07
+ *-----------------------------------------------------------------------------
+*/
+
+//
+// Created by Darker on 2020/9/7.
+//
+
+#ifndef MASTERING_ALGORITHMS_WITH_C_POINTER_H
+#define MASTERING_ALGORITHMS_WITH_C_POINTER_H
+
+#include <stdio.h>
+
+#endif //MASTERING_ALGORITHMS_WITH_C_POINTER_H
